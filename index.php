183029c410d34421c0b2693839221e9c24b1dac1
Simon Ericsson <ericsson.simon@gmail.com>
2012-09-13 09:18:40 +0200

    Change everything

--- index.php
+++ index.php
@@ -1,16 +1,33 @@
-<!DOCTYPE html>
+
+
+<!DOCTYPE>
 <html>
 <head>
-
-<link rel="stylesheet" type="text/css" href="css/style.css" />
-	<title>Välkommna</title> 
+	<title>Simons Super Sida</title>
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
-
 <body>
-
-<h1>tjo</h1>
-
-<p>Hejsvejs</p>
-
+<div id="mainArea">
+	<div id="contentBox">
+        <div id="title">Simons Smarta Sida</div>
+        
+        <div id="linkGroup">
+            <div class="link"><a href="index.html">Start</a></div>
+            <div class="link"><a href="index.html">Om</a></div>
+            <div class="link"><a href="index.html">Bilder</a></div>
+            <div class="link"><a href="index.html">Kontakt</a></div>
+        </div>
+        
+        <div id="box"> 
+          <div id="header"></div>
+          <div class="contentTitle">Vaelkommen</div>
+            <div class="pageContent">
+              
+            </div>
+            <div id="footer">Hemsidan</a> av Simon Ericsson</a> 
+        </div>
+	</div>
+</div>
 </body>
 </html>
